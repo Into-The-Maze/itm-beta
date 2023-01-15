0e5c4b814418bed8cad22576c4893ee8e009f223
Jomlom <jontyali3@gmail.com>
2023-01-15 01:01:36 +0000

    a start on movement

--- test project/Assets/Scripts/PlayerMovement.cs
+++ test project/Assets/Scripts/PlayerMovement.cs
@@ -11,12 +11,12 @@ public class PlayerMovement : MonoBehaviour
     public float maxSpeed = 10f;
     public float maxStamina = 50f;
     float stamina;
-    string movement = "walking";
+    string movementType = "walking";
 
     public LayerMask wallMask;
+    //public LayerMask playerMask;
     //public LayerMask itemMask;
     //public LayerMask vaultMask;
-    //public LayerMask slowMask;
 
     void Start()
     {
@@ -26,34 +26,53 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        movementType(ref movement, ref stamina, speed, maxStamina);
+        MovementType(ref movementType, ref stamina, speed, maxStamina);
+    }
+
+    static void Movement(ref float speed)
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        float moveDirection = horizontal * 90;
+        if (vertical == 1)
+        {
+            moveDirection = moveDirection / 2;
+        }
+        else if (vertical == -1)
+        {
+            moveDirection = 180;
+            moveDirection -= (horizontal * 45);
+        }
+        //Debug.Log($"{moveDirection}");
+
+        
     }
 
     //changes movement speed and stamina depending on wether play is running, sneaking or walking
-    static void movementType(ref string movement, ref float stamina, float speed, float maxStamina)
+    static void MovementType(ref string movementType, ref float stamina, float speed, float maxStamina)
     {
         if (Input.GetKeyDown("left shift") && stamina > 10)
         {
-            movement = "running";
+            movementType = "running";
         }
         else if (Input.GetKeyDown("left ctrl"))
         {
-            movement = "sneaking";
+            movementType = "sneaking";
         }
-        else if ((Input.GetKeyUp("left ctrl") && movement == "sneaking") || 
-            (Input.GetKeyUp("left shift") && movement == "running") ||
+        else if ((Input.GetKeyUp("left ctrl") && movementType == "sneaking") || 
+            (Input.GetKeyUp("left shift") && movementType == "running") ||
             stamina == 0)
         {
-            movement = "walking";
+            movementType = "walking";
         }   
 
-        if (movement == "running")
+        if (movementType == "running")
         {
             speed = 10f;
             stamina = Mathf.Clamp(stamina - (10 * Time.deltaTime), 0, maxStamina + 1);
             //Debug.Log($"running {stamina}");
         }
-        else if (movement == "sneaking")
+        else if (movementType == "sneaking")
         {
             speed = 3f;
             stamina = Mathf.Clamp(stamina + (10 * Time.deltaTime), 0, maxStamina + 1);

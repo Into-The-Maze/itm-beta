43e9ba290f5b0957087f5bb53c91460e9f593960
Jomlom <jontyali3@gmail.com>
2023-01-17 20:09:48 +0000

    working on acceleration fix

--- test project/Assets/Scripts/PlayerMovement.cs
+++ test project/Assets/Scripts/PlayerMovement.cs
@@ -13,8 +13,8 @@ public class PlayerMovement : MonoBehaviour
 
     public float currentSpeed = 0f;
     public float weight = 10f;
-    public float maxSpeed = 100f;
-    public float acceleration = 0.1f;
+    public float maxSpeed = 1000f;
+    public float acceleration = 100f;
     public float maxStamina = 50f;
     public float stamina;
     public MovementType movementType = MovementType.Walking;
@@ -71,16 +71,14 @@ public class PlayerMovement : MonoBehaviour
         moveVector.y = Mathf.Cos(radiansFromNorth);
         moveVector.x = Mathf.Sin(radiansFromNorth);
         if (isMoving) {
-            Debug.Log(radiansFromNorth);
-            Debug.Log(moveVector);
-            Debug.Log(playerBody.velocity);
-            currentSpeed = Mathf.Clamp(currentSpeed + acceleration, 0, maxSpeed);
+            //Debug.Log($"{radiansFromNorth}, {moveVector}, {playerBody.velocity}");
+            currentSpeed = Mathf.Clamp((currentSpeed + acceleration), 0, maxSpeed - 1);
         }
         else {
-            currentSpeed = Mathf.Clamp(currentSpeed - acceleration, 0, maxSpeed);
-            
+            currentSpeed = Mathf.Clamp((currentSpeed - acceleration), 0, maxSpeed - 1);
         }
-        playerBody.velocity = moveVector * Time.deltaTime * currentSpeed;
+        playerBody.velocity = moveVector * currentSpeed * Time.deltaTime * 1000;
+        //Debug.Log($"{moveVector}, {playerBody.velocity}, {currentSpeed}, {playerBody.velocity.magnitude}, {acceleration}");
     }
 
     //changes movement speed and stamina depending on wether play is running, sneaking or walking

fad695d0f1f65e5dc05dce94136080e20b3b0e16
Jamalam <james@jamalam.tech>
2023-01-17 08:46:37 +0000

    feat: use an enum

--- test project/Assets/Scripts/PlayerMovement.cs
+++ test project/Assets/Scripts/PlayerMovement.cs
@@ -15,9 +15,9 @@ public class PlayerMovement : MonoBehaviour
     public float weight = 10f;
     public float maxSpeed = 10f;
     public float maxStamina = 50f;
-    float stamina;
-    string movementType = "walking";
-    float moveDirection = 0;
+    public float stamina;
+    public MovementType movementType = MovementType.Walking;
+    public float moveDirection = 0;
 
     public LayerMask wallMask;
     //public LayerMask playerMask;
@@ -94,26 +94,26 @@ public class PlayerMovement : MonoBehaviour
     {
         if (Input.GetKeyDown("left shift") && stamina > 10)
         {
-            movementType = "running";
+            movementType = MovementType.Walking;
         }
         else if (Input.GetKeyDown("left ctrl"))
         {
-            movementType = "sneaking";
+            movementType = MovementType.Sneaking;
         }
-        else if ((Input.GetKeyUp("left ctrl") && movementType == "sneaking") || 
-            (Input.GetKeyUp("left shift") && movementType == "running") ||
+        else if ((Input.GetKeyUp("left ctrl") && movementType == MovementType.Sneaking) || 
+            (Input.GetKeyUp("left shift") && movementType == MovementType.Running) ||
             stamina == 0)
         {
-            movementType = "walking";
+            movementType = MovementType.Walking;
         }   
 
-        if (movementType == "running")
+        if (movementType == MovementType.Running)
         {
             speed = 10f;
             stamina = Mathf.Clamp(stamina - (10 * Time.deltaTime), 0, maxStamina + 1);
             //Debug.Log($"running {stamina}");
         }
-        else if (movementType == "sneaking")
+        else if (movementType == MovementType.Sneaking)
         {
             speed = 3f;
             stamina = Mathf.Clamp(stamina + (10 * Time.deltaTime), 0, maxStamina + 1);
@@ -125,5 +125,11 @@ public class PlayerMovement : MonoBehaviour
             stamina = Mathf.Clamp(stamina + (5 * Time.deltaTime), 0, maxStamina + 1);
             //Debug.Log($"walking {stamina}");
         }
+    }
+    
+    private enum MovementType {
+        Walking,
+        Running,
+        Sneaking
     }
 }

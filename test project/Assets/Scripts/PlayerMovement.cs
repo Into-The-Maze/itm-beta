9168f8ee46067c87271807bbdfdeccaf833341ab
Long <84541379+L-o-ng@users.noreply.github.com>
2023-01-17 19:45:02 +0000

    Movement clamp debugged, acceleration working
    Max speed needs some work since the max speed is not actually tied to the value of maxSpeed as far as i can tell

--- test project/Assets/Scripts/PlayerMovement.cs
+++ test project/Assets/Scripts/PlayerMovement.cs
@@ -13,8 +13,8 @@ public class PlayerMovement : MonoBehaviour
 
     public float currentSpeed = 0f;
     public float weight = 10f;
-    public float maxSpeed = 10f;
-    public float acceleration = 5f;
+    public float maxSpeed = 100f;
+    public float acceleration = 0.1f;
     public float maxStamina = 50f;
     public float stamina;
     public MovementType movementType = MovementType.Walking;
@@ -31,7 +31,7 @@ public class PlayerMovement : MonoBehaviour
     }
 
     void Update() {
-        MovementType(ref movementType, ref stamina, ref maxSpeed, maxStamina);
+        ChangeMovementType(ref movementType, ref stamina, ref maxSpeed, maxStamina);
         (bool, float) movement = GetRotation();
         Movement(movement.Item1, movement.Item2, ref currentSpeed, maxSpeed, playerBody, acceleration);
     }
@@ -67,10 +67,10 @@ public class PlayerMovement : MonoBehaviour
 
     //moves the player with wasd at the correct speeds
     static void Movement(bool isMoving, float radiansFromNorth, ref float currentSpeed, float maxSpeed, Rigidbody2D playerBody, float acceleration) {
+        Vector2 moveVector;
+        moveVector.y = Mathf.Cos(radiansFromNorth);
+        moveVector.x = Mathf.Sin(radiansFromNorth);
         if (isMoving) {
-            Vector2 moveVector;
-            moveVector.y = Mathf.Cos(radiansFromNorth); 
-            moveVector.x = Mathf.Sin(radiansFromNorth);
             Debug.Log(radiansFromNorth);
             Debug.Log(moveVector);
             Debug.Log(playerBody.velocity);
@@ -78,12 +78,13 @@ public class PlayerMovement : MonoBehaviour
         }
         else {
             currentSpeed = Mathf.Clamp(currentSpeed - acceleration, 0, maxSpeed);
+            
         }
         playerBody.velocity = moveVector * Time.deltaTime * currentSpeed;
     }
 
     //changes movement speed and stamina depending on wether play is running, sneaking or walking
-    static void MovementType(ref string movementType, ref float stamina, ref float maxSpeed, float maxStamina) {
+    static void ChangeMovementType(ref MovementType movementType, ref float stamina, ref float maxSpeed, float maxStamina) {
         if (Input.GetKeyDown("left shift") && stamina > 10) {
             movementType = MovementType.Running;
         }
@@ -114,7 +115,7 @@ public class PlayerMovement : MonoBehaviour
     }
 
     //enum for movement type
-    private enum MovementType {
+    public enum MovementType {
         Walking,
         Running,
         Sneaking
